338b3c52d2de68a8a042b20fe2d448dbdabf295b
Yafira <yafira.martinez@lc.cuny.edu>
2021-12-16 15:20:41 -0500

    added color reset functionality

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ toggleBtn.addEventListener('click', () => {
         lightenText.classList.add('unselected');
         darkenText.classList.remove('unselected');
     }
+    reset();
 })
 
 hexInput.addEventListener('keyup', () => {
@@ -28,6 +29,7 @@ hexInput.addEventListener('keyup', () => {
     const strippedHex = hex.replace('#', '');
 
     inputColor.style.backgroundColor = "#" + strippedHex;
+    reset();
 })
 
 const isValidHex = (hex) => {
@@ -98,3 +100,10 @@ slider.addEventListener('input', () => {
     alteredColor.style.backgroundColor = alteredHex;
     alteredColorText.innerText = `Altered Color ${alteredHex}`;
 })
+
+const reset = () =>{
+    slider.value = 0;
+    sliderText.innerText=`0%`;
+    alteredColor.style.backgroundColor = hexInput.value;
+    alteredColorText.innerText = `Altered Color ${hexInput.value}`;
+}
